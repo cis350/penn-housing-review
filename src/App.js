a08cc9e44d3e2acfca5f89a907e7664c372bad55
Lilian Li <jieli1017@gmail.com>
2023-03-21 02:17:21 -0400

    update fb posting page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FBPostHeader from './components/FBPostHeader';
+import FBPostPage from './components/FBPostPage';
+
+function App() {
+  return (
+    <div className="container">
+      <FBPostHeader />
+      <FBPostPage />
+    </div>
+    
+  );
+}
+
+export default App;
